1994ff85cec61318f0040f92a47ce5a640335d31
MariannaNido <marianna.nido@live.it>
2021-07-05 20:26:59 +0200

    Fixed and completed some classes

--- src/main/java/mainPackage/parser/ParserFilter.java
+++ src/main/java/mainPackage/parser/ParserFilter.java
@@ -5,10 +5,53 @@
  */
 package mainPackage.parser;
 
+import mainPackage.type.Command;
+import mainPackage.type.GameObject;
+
 /**
  *
- * @author sebre
+ * @author MS_C
  */
 public class ParserFilter {
+    
+    private Command command;
+    
+    private GameObject object;
+    
+    private GameObject inventory_obj;
+
+    // CONSTRUCTOR
+    public ParserFilter(Command command, GameObject object, GameObject inventory_obj) {
+        this.command = command;
+        this.object = object;
+        this.inventory_obj = inventory_obj;
+    }
+    
+    // SETTERS
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void setObject(GameObject object) {
+        this.object = object;
+    }
+
+    public void setInventory_obj(GameObject inventory_obj) {
+        this.inventory_obj = inventory_obj;
+    }
+
+    // GETTERS
+    public Command getCommand() {
+        return command;
+    }
+
+    public GameObject getObject() {
+        return object;
+    }
+
+    public GameObject getInventory_obj() {
+        return inventory_obj;
+    }
+    
     
 }
